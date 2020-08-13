f8814696833f8638f7e92c9dfcb1651693c80920
Kranthi <kranthi.kumar@juiceplus.com>
2020-08-12 19:49:59 -0500

    add new code

--- index.php
+++ index.php
@@ -15,6 +15,12 @@ Enter Second Number:
         $sum =  $number1+$number2;     
 echo "The sum of $number1 and $number2 is: ".$sum;   
 }  
-?>  
+?>
+<form method="post">  
+Enter First Number:  
+<input type="number" name="number1" /><br><br>  
+Enter Second Number:  
+<input type="number" name="number2" /><br><br>  
+<input  type="submit" name="submit" value="Add">  
 </body>  
 </html> 
